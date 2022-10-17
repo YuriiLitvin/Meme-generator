d0ddc2bdc2d0f6a137dc59ad642190ad486ed575
unknown <yurijlitviniii@gmail.com>
2022-10-17 18:16:58 +0300

    style.css: Change tag styles; Header.js: Remove useless div tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import logo from "../images/Troll Face.png";
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="header--left-part">
-                <img className="header--logo" src={logo} alt="meme-generator" />
+        <header className="header">
+                <img className="header--image" src={logo} alt="meme-generator" />
                 <h2 className="header--title">Meme Generator</h2>
-            </div>
-            <p className="header--right-part">React Course - Project 3</p>
-        </div>
+            <p className="header--project">React Course - Project 3</p>
+        </header>
     )
 }
