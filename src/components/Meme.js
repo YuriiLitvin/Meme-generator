57b35f50a75205343642dc2aaf7059d088bafe4a
unknown <yurijlitviniii@gmail.com>
2022-11-28 20:44:30 +0200

    Complete fetch data from api; Complete random image generation

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 export default function Meme() {
+    
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(memesObj => setAllMemes(memesObj.data.memes))
+    }, [])
+
+    function getNewMeme() {
+        const index = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[index].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
     return (
         <main>
-            <form className="form--container">
+            <div className="form--container">
                 <input 
                     className="form--input" 
                     type="text" 
@@ -14,11 +38,15 @@ export default function Meme() {
                     placeholder="Bottom text" />
                 <button 
                     className="form--button" 
-                    type="button"
-                >
+                    type="button" 
+                    onClick={getNewMeme}
+                    >
                     Get a new meme image
                 </button>
-            </form>
+                <div className="meme">
+                    <img src={meme.randomImage} alt="meme image"/>
+                </div>
+            </div>
         </main>
     )
 }
